629d34b5657ee292ed83afa7b37ae81890eda802
KhvanVasilii <49273506+KhvanVasilii@users.noreply.github.com>
2025-03-28 14:41:49 +0900

    Complete lesson-04/01.js

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,11 @@
 #  // должен вернуть `true`.
 */
 
-#
+function includesElement(arr, el) {
+    for (let i = 0; i < arr.length; i++) {
+    	if (arr[i] === el) {
+            return true;
+        }
+    }
+    return false;
+};
